0720960a8285b27664fab07f3c646fcbcb2b03b5
henry mutega <hmutega@gmail.com>
2021-07-07 16:18:44 +0300

    Add serial modification lines to folders.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
  function generate_serial(){
+   var initial_string = document.getElementById('init').value.toUpperCase();
+   var final_string = document.getElementById('final').value.toUpperCase();
    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
    var string_length = 5;
-   var random_string = "FC2";
+   var random_string = "";
    for(var i = 0; i < string_length; i++){
      var randomString = Math.floor(Math.random() * characters.length);
      random_string += characters.substring(randomString,randomString+1);
    }
-   document.getElementById('display').value = random_string;
+   document.getElementById('display').value = initial_string + random_string + final_string;
  }
